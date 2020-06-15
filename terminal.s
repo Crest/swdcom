335c3c789a4d4a835f1e956e2e7fe3d9f70cdb06
Jan Bramkamp <crest@rlwinm.de>
2020-06-15 02:15:52 +0200

    Claim copyright of terminal.s

--- terminal.s
+++ terminal.s
@@ -1,22 +1,28 @@
-@
-@    Mecrisp-Stellaris - A native code Forth implementation for ARM-Cortex M microcontrollers
-@    Copyright (C) 2013  Matthias Koch
-@
-@    This program is free software: you can redistribute it and/or modify
-@    it under the terms of the GNU General Public License as published by
-@    the Free Software Foundation, either version 3 of the License, or
-@    (at your option) any later version.
-@
-@    This program is distributed in the hope that it will be useful,
-@    but WITHOUT ANY WARRANTY; without even the implied warranty of
-@    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-@    GNU General Public License for more details.
-@
-@    You should have received a copy of the GNU General Public License
-@    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-@
+@ swdcom terminal functions
 
-@ Terminalroutinen
+@ Copyright (c) 2020, Jan Bramkamp
+@ All rights reserved.
+@ 
+@ Redistribution and use in source and binary forms, with or without
+@ modification, are permitted provided that the following conditions are met:
+@ 
+@ 1. Redistributions of source code must retain the above copyright notice, this
+@    list of conditions and the following disclaimer.
+@ 
+@ 2. Redistributions in binary form must reproduce the above copyright notice,
+@    this list of conditions and the following disclaimer in the documentation
+@    and/or other materials provided with the distribution.
+@ 
+@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+@ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+@ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+@ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+@ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+@ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 @ Instead of the normal USART these terminal words use a pair of ring buffers
 @ in SRAM. The PC (ab)uses the STLINK/V2 as remote memory access interface.
@@ -152,4 +158,4 @@ serial_emit:
 
 .include "../common/terminalhooks.s"
 
-  .ltorg @ Hier werden viele spezielle Hardwarestellenkonstanten gebraucht, schreibe sie gleich !
+  .ltorg @ Flush constant pool
