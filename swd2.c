b342f735abe19ec5492da4d57e5f852916b64ec2
Jan Bramkamp <crest@rlwinm.de>
2020-06-15 03:07:31 +0200

    Add missing sys/timespec.h include

--- swd2.c
+++ swd2.c
@@ -2,15 +2,16 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/timespec.h>
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-#include <stdarg.h>
 
 // CLOCK_MONOTONIC_FAST is FreeBSD specific.
 // Fall back to CLOCK_MONOTONIC_COARSE if available.
